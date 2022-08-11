71ef96ce564b13f6adcf0396ffe626c62df6742c
PatrickMillado <leepmillado@gmail.com>
2022-08-11 14:11:48 -0800

    added files

---
+++ test.js
@@ -0,0 +1,15 @@
+var  firstName = 'Lee';
+
+if (firstName == 'Lee') {
+    console.log('Hello, Lee!')
+}
+
+//What is the value of z?
+var x = 5;
+var y = 6;
+x = 3 +y
+var z = x + y;
+y = 5 + 2*x
+x = y
+
+console.log("Variable in numeric value", z)
